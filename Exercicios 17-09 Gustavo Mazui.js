a8be23c53df4f954afae79aa9a844eb07416d44e
Gustavo Mazui <gustavopmazui18@gmail.com>
2024-09-17 22:01:46 -0300

    Add files via upload

---
+++ Exercicios 17-09 Gustavo Mazui.js
@@ -0,0 +1,175 @@
+//////////////////////////////////////////////////////////////////////// EXERCICIO 1
+/*
+1)
+const primeiroValor = 3
+const segundoValor = 4
+
+const resultado = primeiroValor + segundoValor 
+
+console.log (resultado) 
+
+// 
+2) 
+const primeiroValor = 3
+const segundoValor = 5
+
+const resultado = primeiroValor * segundoValor / 2
+console.log (resultado) */ 
+
+
+/*
+3)
+const primeiroValor = 5
+const segundoValor = 4
+
+const resultado = primeiroValor - segundoValor * -1
+console.log (resultado) */ 
+
+
+
+/*
+4) const primeiroValor = 234
+const segundoValor = 5
+
+const resultado = primeiroValor % segundoValor
+console.log (resultado) */ 
+
+
+
+
+//////////////////////////////////////////////////////////////////////// EXERCICIO 2
+
+
+ /* 
+ 
+ 1) 
+const primeiroValor = 15
+const segundoValor = 55
+
+const resultado = primeiroValor === segundoValor 
+console.log (resultado) */ 
+
+
+
+/* 
+2) 
+const primeiroValor = 3
+const segundoValor = 5
+
+const resultado = primeiroValor !== segundoValor 
+console.log (resultado) */ 
+
+
+/*
+3)
+const primeiroValor = 3
+const segundoValor = 5
+
+const resultado = primeiroValor > segundoValor 
+console.log (resultado)*/ 
+
+/*
+4) const primeiroValor = 55
+const segundoValor = 80
+
+const resultado = primeiroValor < segundoValor
+console.log (resultado) */ 
+
+///////////////////////////////////////// EXERCICIO 3 
+
+
+
+/*
+1)
+const a = true 
+const b = false 
+const c = true 
+
+const resultado = (a&&b) 
+console.log (resultado) falso */ 
+
+
+
+/* 
+2)
+const a = true 
+const b = false 
+const c = true 
+
+const resultado = (b&&c) 
+console.log (resultado) falso */
+
+
+
+
+/*
+3)
+const a = true 
+const b = false 
+const c = true 
+
+const resultado = (a&&c) 
+console.log (resultado) true */
+
+
+/*
+4)
+const a = true 
+const b = false 
+const c = true 
+
+const resultado = (a&&b&&c) 
+console.log (resultado) false */  
+
+//////////////////////////////////////////// EXERCICIO 4
+
+
+/* 
+1) 
+const a = true 
+const b = false 
+const c = true 
+
+const resultado = (a||b) 
+console.log (resultado)*/ 
+
+
+/* 
+2) 
+const a = true 
+const b = false 
+const c = true 
+
+const resultado = (b||c) 
+console.log (resultado) */
+
+/* 
+3) 
+const a = true 
+const b = false 
+const c = true 
+
+const resultado = (a||c) 
+console.log (resultado) */
+
+/*
+4)
+const a = true 
+const b = false 
+const c = true 
+
+const resultado = (a|| b ||c) 
+console.log (resultado) */
+
+//////////////////////////////////////////// EXERCICIO 5
+
+
+/* const nomeUsuario = prompt ("Qual é o seu nome ?") 
+let idadeUsuario = Number(prompt("Qual é seu ano de nascimento")) 
+let ano = Number(prompt("Qual o ano atual?")) 
+let deMaior = true
+
+console.log ("Nome:"+ nomeUsuario)
+console.log ("Idade:" , ano - idadeUsuario)
+console.log ("É maior de idade?" ,  deMaior)
+console.log ("Idade em 2050:", 2050 - idadeUsuario, "anos de idade")*/
